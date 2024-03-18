fe8127a3a1b10ed37d3ab7778d64a71c096846c6
Dark-feenix <163395608+Dark-feenix@users.noreply.github.com>
2024-03-18 17:41:06 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,63 @@
+/*
+
+
+
+
+
+Project Name : FEENIX - MD
+Creator : @Dark-feenix , @dark-sasiya 
+
+*/
+
+module.exports = {
+  OWNER_NUMBER: "94722807735",
+  SUDO_NUMBERS : "94722807735",
+  OWNER_NAME: "『 𝙼𝚁 𝚂𝙰𝚂𝙸𝚈𝙰 』",
+  BOT_NAME : "F E E N I X",
+  ZUSYCO_FILE : "FEENIX-ID-QR",
+  LOGOS: {
+  
+  },
+  STOP_SEND : {
+        WELCOME_MESSAGE : false,
+        GOODBYE_MESSAGE : false
+    },
+  DESABLE_WELCOME_GOODBYE_JIDS : [],
+  TIME_ZONE :  "Asia/Colombo",
+  WORK_TYPE : "public",
+  WORK_MODE : "onlygroups",
+  WORK_MODE_MESSAGE_SEND : false,
+  CAPTION : '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ꜰᴇᴇɴɪx ᴍᴅ ʙᴏᴛ',
+  STICKER_PACK : '[ FEENIX ]',
+  BAD_WORDS : 'fuck,pussy',
+  ANTI_BAD : false,
+  ANTI_LINKS : 'chat.whatsapp.com',
+  ANTI_LINK : false,
+  ANTI_LINK_KICK : false,
+  MAX_SIZE : 100,
+  MOROCCO_NUMBERS : {
+        BLOCK : false,
+        NOT_WORKING : false,
+  },
+  READ : {
+     ALL_MESSAGES : true,
+     ONLY_COMMANDS : false,
+     STATUS : false,
+  },
+  BOT_OFFLINE : false,
+  INBOX_BLOCK : false,
+  AUTO_REACT : false,
+  OWNER_REACT : '🤵‍♂️',
+  AUTO_BGM : false,
+  BGM : {
+    
+  },
+  AUTO_STICKER : false,
+  STICKERS : {
+    "pit" : "https://i.ibb.co/NVLMQDP/pit.webp",
+    "agei" : "https://i.ibb.co/yqk8QQ9/agei.webp",
+    "ne" : "https://i.ibb.co/N6Wb6mZ/ne.webp",
+  },
+  LOAD_ANIMATION : false,
+ 
+};
